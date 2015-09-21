3e14f5a974849dd5e097e9bbb63b4aa8bb665127
AMK <amk@localhost.localdomain>
2015-09-22 00:11:44 +0200

    Adding comments; minor thouches with the centrality function

--- scenesorter.py
+++ scenesorter.py
@@ -1,14 +1,8 @@
 import string
 import numpy as np
 
-def gettokens(sentences):
-    terms = []
-    for s in sentences:
-        words = s.split()
-        for w in words:
-            terms.append(w)
-    return set(terms)
-
+""" FUNCTIONS """
+# Word vectors generation
 def word2vecindexing(sentences):
     model2001 = open("model2001").readlines()[2:]
     word2vecindex = dict()
@@ -26,11 +20,24 @@ def randomindexing(sentences, vecsize):
         randomindex[t] = np.random.randn(vecsize)
     return randomindex
 
+# Helper function for random indexing, return set of unique words
+def gettokens(sentences):
+    terms = []
+    for s in sentences:
+        words = s.split()
+        for w in words:
+            terms.append(w)
+    return set(terms)
+
+# Constructs document vectors by adding word vectors up
+# current options are random indexing and Googles word2vec model
 def builddocumentvectors(sentences, vecsize=100, indexing="rand"):
     if indexing == "rand":
         randomindex = randomindexing(sentences, vecsize)
     elif indexing == "word2vec":
         randomindex, vecsize = word2vecindexing(sentences)
+    else:
+        raise ValueError("Incorrect indexing model chosen!")
     documentvectors = []
     for s in sentences:
         sentvec = np.zeros(vecsize)
@@ -40,18 +47,26 @@ def builddocumentvectors(sentences, vecsize=100, indexing="rand"):
         documentvectors.append(sentvec)
     return(documentvectors)
 
-def closenesscentrality(documentvectors):
-    closenessindexdict = dict()
+# Assigns a single semantic score per document; two options
+def centrality(documentvectors, state="closeness"):
+    centralitydict = dict()
     for i, d in enumerate(documentvectors):
         similarityscores = []
         for s in documentvectors:
             similarityscores.append(cosine(d, s))
-        closenessindexdict[i] = 1 / sum(similarityscores)
-    return closenessindexdict
+            if state == "closeness":
+                centralitydict[i] = 1 / sum(similarityscores)
+            elif state == "hamonic": # A variant of closeness
+                centralitydict[i] = sum(map(lambda x: 1/x, similarityscores))
+            else:
+                raise ValueError("Incorrect centrality state chosen!")
+    return centralitydict
 
+# Similarity messure; cosine distance
 def cosine(u, v):
     return  np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
+# Sort timestamp and sentence lists by semantic scores.
 def sorter(timecodes, sentences, similarityscores):
     timesort, sentsort = [], []
     ordertupl = sorted(similarityscores.items(), key=lambda x: x[1])
@@ -60,16 +75,23 @@ def sorter(timecodes, sentences, similarityscores):
         sentsort.append(sentences[t[0]])
     return timesort, sentsort
 
-srt = open("2001.A.Space.Odyssey.1968.720p.HDDVD.x264-hV.srt").readlines()
-punct = string.punctuation
-times = srt[0::4]
-sents = [''.join(char for char in sentstring 
-                      if char not in punct).strip().lower() 
-                 for sentstring in srt[2::4]]
+""" RUNTIME """
+def main():
+    # Initial variables
+    srt = open("2001.A.Space.Odyssey.1968.720p.HDDVD.x264-hV.srt").readlines()
+    punct = string.punctuation
+    times = srt[0::4]
+    sents = [''.join(char for char in sentstring 
+                        if char not in punct).strip().lower() 
+                    for sentstring in srt[2::4]]
 
-documentvectors = builddocumentvectors(sents, indexing='word2vec')
-simscores = closenesscentrality(documentvectors)
-s_times, s_sents = sorter(times, sents, simscores)
+    # Doing all the work; remember to set the state and indexing
+    documentvectors = builddocumentvectors(sents, indexing='word2vec')
+    simscores = centrality(documentvectors, state='closeness')
+    s_times, s_sents = sorter(times, sents, simscores)
 
-for i in range(len(s_sents)):
-    print(s_times[i], s_sents[i])
+    # Output data
+    for i in range(len(s_sents)):
+        print(s_times[i], s_sents[i])
+
+main()
