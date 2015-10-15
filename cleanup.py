df6ed3b732c0d0b05f72a3cfddef9acd0b09857a
amk <amk@localhost.localdomain>
2015-10-15 19:10:40 +0200

    Baby steps towards expanded training data

--- cleanup.py
+++ cleanup.py
@@ -1,8 +1,13 @@
-srt = open("2001.A.Space.Odyssey.1968.720p.HDDVD.x264-hV.srt").readlines()
-sents = srt[2:10:4]
 punct = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-cleansents = [''.join(char for char in sentstring if char not in punct).strip().lower() for sentstring in sents]
-with open("2001sents.txt", "w") as f:
-    for i in cleansents:
-        f.write("%s\n" % i)
-    f.close()
+
+def subcleanup():
+    srt = open("2001.A.Space.Odyssey.1968.720p.HDDVD.x264-hV.srt").readlines()
+    sents = srt[2:10:4]
+    cleansents = [''.join(char for char in sentstring if char not in punct).strip().lower() for sentstring in sents]
+    with open("2001sents.txt", "w") as f:
+        for i in cleansents:
+            f.write("%s\n" % i)
+        f.close()
+
+def scriptclean()
+    script = open("data/Script___2001___A_Space_Odyssey.txt").read().lower().split()
